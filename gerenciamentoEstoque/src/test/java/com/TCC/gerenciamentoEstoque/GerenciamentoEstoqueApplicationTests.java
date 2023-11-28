8a624deb643120992d921f4f3148fcbc5b7740d9
Matheus <rsantosmatheus@outlook.com>
2023-11-28 10:39:16 -0300

    Classes iniciais

---
+++ gerenciamentoEstoque/src/test/java/com/TCC/gerenciamentoEstoque/GerenciamentoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TCC.gerenciamentoEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
